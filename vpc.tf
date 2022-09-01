913eafc6ecee05a0abfa372097b08581530ef36f
xuja <jinan.koh@gmail.com>
2022-09-01 16:38:41 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "network" {
+    source  = "app.terraform.io/xuja/network/google"
+    version = "~> 3.0"
+
+    project_id   = "p-btqjaurbxgo5-0"
+    network_name = "example-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+
+
+}
